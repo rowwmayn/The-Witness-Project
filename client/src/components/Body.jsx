1355211df49b25a02c965d308f08c241ecb4d7ef
Roman <romerantshere@gmail.com>
2025-02-22 10:52:38 +0600

    Initial structure done

---
+++ client/src/components/Body.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import "../App.css"
+const Body = () => {
+  return (
+    <section className="body">
+        <div className='body-container'>
+            <h3>Lorem</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At, odit.</p>
+            
+            
+            
+            
+            <div className='image'>
+                <h2>Image</h2>
+
+            </div>
+
+        </div>
+    </section>
+  )
+}
+
+export default Body
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../App.css"
+const Header = () => {
+  return (
+    <nav className="navbar">
+      <img src="/logo.png" alt="Logo" className="logo" />
+      <p>Hello I am the header</p>
+    </nav>
+  )
+}
+
+export default Header
